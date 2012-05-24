dc0934b599809a13d3582f77434fd5eed4f6188d
Mike Kusold <kusold@gmail.com>
2012-05-24 14:50:10 -0400

    Added instructions

--- rating-stars.rb
+++ rating-stars.rb
@@ -1,3 +1,24 @@
+# Title: Rating Stars
+# Author: Mike Kusold http://mikekusold.com
+# Description: A plugin that allows the author to assign a 0-5 star ranking to whatever he chooses.
+#
+# Credits: This code is strongly based around how KomodoMedia tackled a similar problem in their
+#          blog post: http://www.komodomedia.com/blog/2006/01/css-star-rating-part-deux/
+#
+# Syntax {% rating 0.0-5.0 %}
+#
+# Example:
+# {% rating 3.5 %}
+#
+# Output:
+# <div class='star-rating' style='width: 50px;'>
+# <div class='current-rating' style='width: 70px;'></div></div>
+#
+# Installation: Copy rating-stars.rb into your Plugin/ directory.
+#               Copy rating-stars.css into your Stylesheets/ directory
+#               Modify your .themes/classic/source/custom/head.html file to include the rating-stars.css file
+#
+
 module Jekyll
   class RatingStars < Liquid::Tag
 
